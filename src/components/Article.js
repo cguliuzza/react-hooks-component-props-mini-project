6de22f1bce287767a8aa46b4321fcad4bc3bce75
christinaguliuzza <christina.guliuzza@gmail.com>
2021-09-02 10:13:35 -0700

    Completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Article = ({ id, title, date="January 1, 1970", preview }) => {
+
+    return (
+        <article key={id}>
+            <h3>{ title }</h3>
+            <small>{ date }</small>
+            <p>{ preview }</p>
+        </article>
+    )
+}
+
+export default Article
